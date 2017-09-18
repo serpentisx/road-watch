35d34bf2327c27fe67d3b44a19ad79c92ce98d91
Hinrik Snær <hinriksnaer@gmail.com>
2017-09-18 17:39:20 +0000

    Added getters for Municipality and Region classes

--- src/main/java/app/model/Municipality.java
+++ src/main/java/app/model/Municipality.java
@@ -5,4 +5,26 @@ import java.util.List;
 public class Municipality {
     private String name;
     private List<Post> posts;
+    
+    public Municipality (String name){
+        this.name = name;
+        //Kannski viljum við hafa call by need frekar en pre-load
+        this.posts = generatePosts(name);
+    }
+    
+    //Input: string name representing a valid municipality name in the database
+    //returns: List of Post objects that have been made for said municipality
+    private List<Post>  generatePosts (String name){
+        return null;
+    }
+
+    //return the name
+    public String getName() {
+        return name;
+    }
+
+    //return the posts
+    public List<Post> getPosts() {
+        return posts;
+    }
 }
--- src/main/java/app/model/Region.java
+++ src/main/java/app/model/Region.java
@@ -1,14 +1,36 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package app.model;
 
-/**
- *
- * @author Hinri
- */
+//Class for basic information for Regions and their municipalities
+
+import java.util.List;
+
 public class Region {
+    private String name;
+    private List<Municipality> municipalities;
     
+    public Region (String name) {
+        this.name = name;
+        //Kannski viljum við hafa call by need frekar en pre-load
+        this.municipalities = generateMunicipalities(name);
+    }
+    
+    //Input: string name representing a valid region name in the database
+    //returns: List of municipality objects that are located in said region
+    public List<Municipality> generateMunicipalities(String name){
+        return null;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @return the municipalities
+     */
+    public List<Municipality> getMunicipalities() {
+        return municipalities;
+    }
 }
