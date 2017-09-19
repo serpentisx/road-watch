cc6801038a5273abf2c63bf642ef6623eb56b3c6
Huy Nguyen <huy_van1@live.com>
2017-09-19 10:41:04 +0000

    Added login/register page

--- src/main/java/app/controller/MainController.java
+++ src/main/java/app/controller/MainController.java
@@ -5,8 +5,11 @@
  */
 package app.controller;
 
+import app.model.User;
+import app.services.UserService;
+import java.util.Map;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -25,10 +28,41 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RequestMapping("") // Request Mapping for the root
 public class MainController {
     
+    @Autowired
+    UserService userService;
+    
     // Route for home page 
     // @return String filePath.  filePath is supposed to point to a .jsp file.
     @RequestMapping("/")
     public String main_page(){
-        return "main_page";
+        return "login_register_page";
+    }
+    
+    @RequestMapping(value = "/posts", method = RequestMethod.POST)
+    public String login(
+        @RequestParam Map<String,String> params, ModelMap model) {
+        String name = params.get("login_name");
+        String password = params.get("login_password");
+
+        if (userService.validateUser(name, password)) {
+            return "posts_page";
+        }
+        else {
+            model.addAttribute("invalid_input", "Rangt notandanafn eða lykilorð");
+            return "login_register_page";
+        }
+    }
+    
+    @RequestMapping(value = "/register", method = RequestMethod.POST)
+    public String register (
+        @RequestParam Map<String,String> params, ModelMap model) {
+        String name = params.get("register_name");
+        String password = params.get("register_password");
+        String email = params.get("register_email");
+ 
+        User user = new User(name, password, email);
+        userService.addUser(user);
+
+        return "login_register_page";
     }
 }
