43b1a21da53da1983f28246f3e99c4fd1520c17d
serpentisx <huy_van1@live.com>
2017-09-20 17:00:04 +0000

    Added documentation and formatted

--- src/main/java/app/model/Location.java
+++ src/main/java/app/model/Location.java
@@ -1,13 +1,27 @@
 package app.model;
 
-// Class for requred location data
-public class Location {
-    //Coordinates should have two values [0] for x axis and [1] for y axis
+/**
+ * @author Team 20 HBV501G - Fall 2017
+ *
+ * Location stores information about one location, its coordinates, address
+ * area code, municipality and region.
+ */
 
+public class Location {
+    
+    // Coordinates should have two values [0] for x axis and [1] for y axis
     private double[] coordinates;
+    
+    // Address of this location
     private String address;
+    
+    // Area code of this location
     private int zip;
+    
+    // The municipality that this location belongs to
     private String municipality;
+    
+    // The region that this location belongs to
     private String region;
 
     public Location(double[] coordinates, String address, int zip, String municipality, String region) {
