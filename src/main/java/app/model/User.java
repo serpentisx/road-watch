59a3b50cee4c01b24eafa2e86bb039ffeaf5e7a4
Huy Nguyen <huy_van1@live.com>
2017-09-20 17:58:15 +0000

    Update User.java

--- src/main/java/app/model/User.java
+++ src/main/java/app/model/User.java
@@ -1,16 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package app.model;
 
 /**
+ * @author Team 20 HBV501G - Fall 2017
  *
- * @author Huy Van Nguyen
+ * Model for the user. 
+ * Stores login information and user's email
  */
 public class User {
-    
+ 
     private String username;
     private String userPassword;
     private String userEmail;
