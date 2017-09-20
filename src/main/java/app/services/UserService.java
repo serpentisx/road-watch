1ec4e61a8b5a6318c86842038116904e4b33cc33
Huy Nguyen <huy_van1@live.com>
2017-09-20 18:01:04 +0000

    Update UserService.java

--- src/main/java/app/services/UserService.java
+++ src/main/java/app/services/UserService.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package app.services;
 
 import app.model.User;
@@ -10,15 +5,35 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 
 /**
+ * @author Team 20 HBV501G - Fall 2017
  *
- * @author Huy Van Nguyen
+ * A service class for users
+ * 
+ * This is a interface that defines methods that all service classes for users 
+ * should meet
  */
 
 public interface UserService {
     
+    /**
+    * Checks for a user existence in the repository
+    * 
+    * @param String username:  the user's username
+    * @param String password:  the user's password
+    */
     public boolean validateUser(String username, String password);
 
+    /**
+    * Adds a user to the repository
+    * 
+    * @param User user:  user to be added to the repository
+    */
     public void addUser(User user);
 
+    /**
+    * Get all users in the repository
+    * 
+    * @return List<User>:  a list of users.
+    */
     public List<User> getAllUsers();
 }
