6c6bbaee3689149767348a8d5f9bf0a588a1157b
Valentin Oliver <valentin081194@gmail.com>
2017-10-13 21:39:39 +0000

    small fix

--- src/main/java/app/model/Post.java
+++ src/main/java/app/model/Post.java
@@ -13,7 +13,7 @@ import javax.persistence.Table;
 /**
  * @author Team 20 HBV501G - Fall 2017
  * @author Bjarki Viðar Kristjánsson (bvk1@hi.is)
- * @author Hinrik Snær Guðmundsson (hsg30.is)
+ * @author Hinrik Snær Guðmundsson (hsg30@hi.is)
  * @author Huy Van Nguyen (hvn1@hi.is)
  * @author Valentin Oliver Loftsson (vol1@hi.is)
  *
--- src/main/java/app/service/ServiceImplementation.java
+++ src/main/java/app/service/ServiceImplementation.java
@@ -14,7 +14,7 @@ import org.springframework.stereotype.Service;
 /**
  * @author Team 20 HBV501G - Fall 2017
  * @author Bjarki Viðar Kristjánsson (bvk1@hi.is)
- * @author Hinrik Snær Guðmundsson (hsg30.is)
+ * @author Hinrik Snær Guðmundsson (hsg30@hi.is)
  * @author Huy Van Nguyen (hvn1@hi.is)
  * @author Valentin Oliver Loftsson (vol1@hi.is)
  */
