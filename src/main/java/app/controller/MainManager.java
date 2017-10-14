99a731005c65864e4ff08542322e795f18524009
serpentisx <huy_van1@live.com>
2017-10-14 15:05:26 +0000

    Added email-contact functionality

--- src/main/java/app/controller/MainManager.java
+++ src/main/java/app/controller/MainManager.java
@@ -2,7 +2,10 @@
 package app.controller;
 
 import app.model.Post;
+import app.service.Mail;
 import app.service.PostService;
+import com.sun.mail.util.MailSSLSocketFactory;
+import java.security.GeneralSecurityException;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -10,6 +13,8 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpSession;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  * @author Team 20 HBV501G - Fall 2017
@@ -41,6 +46,23 @@ public class MainManager {
         
         model.addAttribute("posts", posts);
         model.addAttribute("postsJSON", postsJSON);
+        return "index";
+    }
+    
+    @RequestMapping("/mail")
+    public String renderPage(HttpSession session, ModelMap model) throws GeneralSecurityException {
+        model.addAttribute("username", (String) session.getAttribute("loggedInUsername"));
+        ApplicationContext context =
+             new ClassPathXmlApplicationContext("Spring-Mail.xml");
+
+        MailSSLSocketFactory socketFactory= new MailSSLSocketFactory();
+socketFactory.setTrustAllHosts(true);
+
+    	Mail mm = (Mail) context.getBean("mailMail");
+        mm.sendMail("vegavaktin@gmail.com",
+    		   "hvn1@hi.is",
+    		   "vegavaktin test",
+    		   "Testing only \n\n Hello Spring Email Sender");
         return "index";
     }
 }
