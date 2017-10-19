64e39cfd9aedfa2d9a13b09d9394bf72b9a04d54
Valentin Oliver <valentin081194@gmail.com>
2017-10-19 23:14:40 +0000

    small fix

--- src/main/java/app/model/Post.java
+++ src/main/java/app/model/Post.java
@@ -1,7 +1,6 @@
 package app.model;
 
 import java.time.LocalDate;
-import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
 import java.util.Locale;
